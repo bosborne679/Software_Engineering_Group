2cb3a7bde75d47aaf55023942d87a306474fd0a1
willC707 <willc@will-Laptop>
2022-07-06 09:19:49 -0400

    web server start. Need some work still

---
+++ MCS/tornado/web_server.py
@@ -0,0 +1,42 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import asyncio
+import sys
+import os.path
+
+
+
+
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("This is the Back End Test Page")
+
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("mcspage.html")
+
+        
+        if self.get_argument("input1") is not None:
+            value = self.get_argument("input1")
+            
+            print(value)
+        else:
+            pass
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers =[
+        (r"/",basicRequestHandler),
+        (r"/home", staticRequestHandler)
+        ]
+        
+        tornado.web.Application.__init__(self, handlers)
+if __name__ == "__main__":
+    app = Application()
+    app.listen(8888)
+    print("Listening to port 8888")
+    tornado.ioloop.IOLoop.current().start()
+
+
